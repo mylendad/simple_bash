af10268b7e0f942bfe1287a7329b002a9973c5c8
mylendad <karpov.vlg@gmail.com>
2024-11-23 14:28:55 +0300

    upgrade -e

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -6,26 +6,26 @@
 // #include <getopt.h>
 // #include "grep.h"
 
-// flags parser(int args, char **argv) {
+// flags parser(int argc, char **argv) {
 //     flags argument = {0}; 
-//     int arguments = getopt(args, argv, "e:ivclnhsf:o");
+//     int arguments = getopt(argc, argv, "e:ivclnhsf:o");
 //     while ((arguments) != -1) { // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg. 
 //         switch (arguments)
 //         {
 //             case 'e':
 //                 argument.e = 1;
-//                 argument.reg_pattern = optarg; // // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg.
+//                 argument.reg_pattern[0] = optarg; // // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg.
 //                 break;
 //             case 'count_filename':
 //                 argument.count_filename = REG_ICASE; // не учитывать регистр
 //                 break;
 //             case 'v':
 //                 argument.v = 1;
-//                 argument.reg_pattern = optarg;
+//                 argument.reg_pattern[0] = optarg;
 //                 break;
 //             case 'c':
 //                 argument.c = 1;
-//                 argument.reg_pattern = optarg;
+//                 argument.reg_pattern[0] = optarg;
 //                 break;
 //             // case 'n':
 //             //     argument.s = 1;
@@ -50,8 +50,8 @@
 //                 exit(1);
 //             }
 //         }
-//         if (argument.reg_pattern == NULL) { // // на слчай если регулярное выражение не распарсилось с помощью флагов
-//                 argument.reg_pattern = argv[optind]; // optind — это индекс следующего обрабатываемого элемента argv. Изначально "1"
+//         if (argument.reg_pattern[0] == NULL) { // // на слчай если регулярное выражение не распарсилось с помощью флагов
+//                 argument.reg_pattern[0] = argv[optind]; // optind — это индекс следующего обрабатываемого элемента argv. Изначально "1"
 //                 optind++;
 //             }
 //     return argument;
@@ -104,7 +104,7 @@
 //     argument = parser(argc, argv); // why?
 //     regex_t reg; // аргумент preg в  regcomp, regexec (хранение буфергого шаблона, указатель на заканчивающуюся null строку regex и флаги cflags, используемые для определения типа компиляции.)
    
-//     if (regcomp(&reg, argument.reg_pattern, argument.count_filename) != 0) { // regcomp если больше 0 то ошибка
+//     if (regcomp(&reg, argument.reg_pattern[0], argument.count_filename) != 0) { // regcomp если больше 0 то ошибка
 //         perror ("ErrorOutput");
 //        return;
 //     }
@@ -125,50 +125,50 @@
 
 #define MAX_LINE_SIZE 1024
 
-flags parser(int args, char **argv) {
+flags parser(int argc, char **argv) {
     flags argument = {0}; 
     int arguments;
-    while ((arguments = getopt(args, argv, "e:ivclnhsf:o")) != -1) { // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg. 
+    while ((arguments = getopt(argc, argv, "e:ivclnhsf:o")) != -1) { // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg. 
         switch (arguments)
         {
             case 'e':
                 argument.e = 1;
-                argument.reg_pattern = optarg; // // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg.
+                add_pattern(&argument, optarg); // // Если за таким символом стоит двоеточие, то параметр требует указания аргумента. При этом getopt() помещает указатель на следующий за символом параметра текст в тот же элемент argv, или на текст следующего элемента argv в optarg.
                 break;
             case 'i':
                 argument.i = REG_ICASE; // не учитывать регистр
                 break;
             case 'v':
                 argument.v = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg; // optarg
                 break;
             case 'c':
                 argument.c = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break;
             case 'n':
                 argument.n = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break;
             case 'l':
                 argument.l = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break;
             case 'h':
                 argument.h = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break;
             case 's':
                 argument.s = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break;   
             case 'f':
                 argument.f = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break; 
             case 'o':
                 argument.o = 1;
-                argument.reg_pattern = optarg;
+                argument.reg_pattern[0] = *optarg;
                 break;
             default:
                 perror("ErrorParser");
@@ -177,16 +177,28 @@ flags parser(int args, char **argv) {
             // printf("%s:", *argv);
         }
         // printf("%s:", *argv);
-        if (argument.reg_pattern == NULL && optind < args) { // // на слчай если регулярное выражение не распарсилось с помощью флагов
-            argument.reg_pattern = argv[optind]; // optind — это индекс следующего обрабатываемого элемента argv. Изначально "1"
+        if (argument.reg_pattern[0] == 0 && optind < argc) { // // на слчай если регулярное выражение не распарсилось с помощью флагов
+            add_pattern(&argument, argv[optind]);
+            argument.reg_pattern[0] = *argv[optind]; // optind — это индекс следующего обрабатываемого элемента argv. Изначально "1"
             optind++;
             }
-        if (argument.reg_pattern == NULL) {
+        if (argument.reg_pattern[0] == 0) {
             perror("No pattern.\n");
             exit(1);
     }
     return argument;
 }
+
+void add_pattern(flags *argument, char *pattern) {
+    if (argument->len != 0) {
+        strcat(argument->reg_pattern + argument->len, "|");
+        argument->len++;
+    }
+    argument->len = argument->len + (sprintf(argument->reg_pattern + argument->len, "(%s)", pattern));
+    printf("%s!!!\n", argument->reg_pattern);
+    printf("%d!!!\n", argument->len);
+}
+
 
 void outline(char *line, int n){
     for (int count_filename = 0; count_filename < n; count_filename++) {
@@ -200,17 +212,23 @@ void outline(char *line, int n){
     // }
 }
 
-void comparator(flags *argument, char *file_name, regex_t *reg, char **argv) {
-    FILE* f = fopen(file_name, "r");
+FILE *reader(flags *argument, char *file_name) {
+       FILE* f = fopen(file_name, "r");
     if (f == NULL) {
         if (argument->s == 0) {
         
             perror(file_name);
             
         }
-        return;
+        exit(1);
     }
+    return f;
+}
 
+
+void comparator(flags *argument, char *file_name, regex_t *reg, char **argv) { // regex_t *reg - структура для хранения скомпилированного регулярного выражения.
+ 
+    FILE *f = reader(argument, file_name);
     char line[MAX_LINE_SIZE];
     int count_reg = 0; 
     int line_counter = 1;
@@ -228,8 +246,12 @@ void comparator(flags *argument, char *file_name, regex_t *reg, char **argv) {
             
             if (((result == 0  && argument->v == 0) || (result != 0 && argument->v == 1)) && argument->c == 0 && argument->o == 0){
         
-                if (argv[3] != NULL && argument->h == 0){
-                    printf("%s:", file_name);
+                if (argv[3] != NULL  && argument->h == 0){
+                    if (strstr("-", argv[3]) == NULL) {
+                        
+                        printf("%s:", file_name);
+                    }
+                    
                 }
 
                  if (argument->n == 1) {
@@ -275,16 +297,28 @@ void comparator(flags *argument, char *file_name, regex_t *reg, char **argv) {
         
             if (result == 0  && argument->o == 1) {
                 printf("%s\n", argv[2]);
+                count_reg++;
             }
+
+            if (argument->c == 1 && result == 0) {
+            
         }
        
-        if (argument->c == 1) {
+        if (argument->c == 1 && result == 0) {
             // printf("%s:", *argv);
             // printf("%s:", *argv);
-            printf("%d\n", count_reg);
+            // if (argv[3] != NULL  && argument->h == 0){
+            //         if (strstr("-", argv[3]) == NULL) {
+                    count_reg++;
+            //             printf("%s:", file_name);
+            //         }
+                       
             // printf("%s:", *argv);
+                }
+                
+         
         }
-        
+        printf("%d\n", count_reg);
         
             fclose(f);
     
@@ -295,8 +329,8 @@ void comparator(flags *argument, char *file_name, regex_t *reg, char **argv) {
 void output (flags *argument, int argc, char **argv) {
     
     regex_t reg; // аргумент preg в  regcomp, regexec (хранение буфергого шаблона, указатель на заканчивающуюся null строку regex и флаги cflags, используемые для определения типа компиляции.)
-   
-    if (regcomp(&reg, argument->reg_pattern, argument->i) != 0) { // regcomp если больше 0 то ошибка; argument->count_filename - флаг в функции regcomp
+    printf("%s", argument->reg_pattern);
+    if (regcomp(&reg, argument->reg_pattern, REG_EXTENDED | argument->i) != 0) { // regcomp если больше 0 то ошибка; argument->count_filename - флаг в функции regcomp; | побитовая операция
         perror ("ErrorOutput");
        return;
     }
@@ -304,12 +338,13 @@ void output (flags *argument, int argc, char **argv) {
 //  char *file_name = argv[count_filename];
     
     for (count_filename = optind; count_filename < argc; count_filename++) {  // optind
-            // printf("%s:", argv[count_filename]);
-        //     if (argv[4] != NULL) {
-        //     //  printf("%s:", argv[count_filename]);
-        //     // char *file_name = argv[count_filename];
-        // printf("%s:", file_name);
-        // }
+            
+           if (argv[4] != NULL  && argument->h == 0){ // for flag -c
+                    if (strstr("-", argv[4]) == NULL) {
+                printf("%s:", argv[count_filename]);
+            }
+        }
+        
         comparator(argument, argv[count_filename], &reg, argv);
     }
     regfree(&reg); // Предоставив regfree предварительно обработанный буферный шаблон, preg освободит память, отведенную этому шаблону во время процесса компиляции regcomp.
--- src/grep/grep.h
+++ src/grep/grep.h
@@ -2,18 +2,24 @@
 #define GREP_H
 
 #include <regex.h>
+
 #define MAX_LINE_SIZE 1024
 
 typedef struct flags
 {
-    char *reg_pattern;
+    int len;
+    // char pattern_e[MAX_LINE_SIZE];
+    char reg_pattern[MAX_LINE_SIZE];
     int e, i, v, c, l, n, h, s, f, o;
 }flags;
 
+void add_pattern(flags *argument, char *pattern);
+
 flags parser(int args, char **argv);
 
-
 void outline(char *line, int n);
+
+FILE *reader(flags *argument, char *file_name);
 
 void comparator(flags *argument, char *file, regex_t *reg, char **argv);
 
